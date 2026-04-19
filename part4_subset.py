89a6f52f055ab49264122686205e5cea6c256d66
Ammaar Saadat <saadatam@umich.edu>
2026-04-19 14:26:43 -0400

    finished logic for pt4.

--- part4_subset.py
+++ part4_subset.py
@@ -12,3 +12,20 @@ cur = con.cursor()
 
 # 1) query following - all melanoma PBMC samples at time_from_treatment_start = 0 from patients who have been treated with miraclib. 
 cur.execute("SELECT * FROM cells WHERE condition = 'melanoma' AND sample_type = 'PBMC' AND treatment = 'miraclib' AND time_from_treatment_start = '0'")
+res_samples = cur.fetchall()
+
+# 2) extension number of samples from each project
+cur.execute("SELECT project, COUNT(*) AS sample_count FROM cells WHERE condition = 'melanoma' AND sample_type = 'PBMC' AND treatment = 'miraclib' AND time_from_treatment_start = '0' GROUP BY project;")
+res_sample_count = cur.fetchall()
+
+# 3) number of subjects that were responders / non-responders
+cur.execute("SELECT response, COUNT(DISTINCT subject) AS subject_count FROM cells WHERE condition = 'melanoma' AND sample_type = 'PBMC' AND treatment = 'miraclib' AND time_from_treatment_start = '0' GROUP BY response;")
+res_responders = cur.fetchall()
+
+# 4) number of subjects that were male / female
+cur.execute("SELECT sex, COUNT(DISTINCT subject) AS sample_count FROM cells WHERE condition = 'melanoma' AND sample_type = 'PBMC' AND treatment = 'miraclib' AND time_from_treatment_start = '0' GROUP BY sex;")
+res_sex = cur.fetchall()
+
+# 5) considering melanoma males, avg number of b cells for responders at t=0
+cur.execute("SELECT ROUNT(AVG(b_cell), 2) FROM cells WHERE condition = 'melanoma' AND sex = 'M' AND response = 'yes' AND time_from_treatment_start = '0';")
+res_average_mel_mals = cur.fetchall()
